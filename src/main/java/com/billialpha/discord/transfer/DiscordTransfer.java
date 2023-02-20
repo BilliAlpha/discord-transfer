95efcdf5022bbce017988812bff584c8816718fe
BilliAlpha <billi.pamege.300@gmail.com>
2023-02-20 17:56:52 +0100

    Fix crash when failing to add migrated emote
    Fixes #18: Crash when trying to migrate a message with the maximum number of reactions

--- src/main/java/com/billialpha/discord/transfer/DiscordTransfer.java
+++ src/main/java/com/billialpha/discord/transfer/DiscordTransfer.java
@@ -48,7 +48,7 @@ import java.util.function.Function;
  * @author BilliAlpha <billi.pamege.300@gmail.com>
  */
 public class DiscordTransfer {
-    public static final String VERSION = "2.2.1";
+    public static final String VERSION = "2.2.2";
     private static final Logger LOGGER = LoggerFactory.getLogger(DiscordTransfer.class);
     private final GatewayDiscordClient client;
     private final Guild srcGuild;
@@ -299,7 +299,11 @@ public class DiscordTransfer {
         .subscribe(
                 createdMessage -> msg.addReaction(MIGRATED_EMOJI)
                     .doOnSuccess(h -> fut.complete(createdMessage))
-                    .doOnError(fut::completeExceptionally)
+                    .doOnError(err -> {
+                        LOGGER.warn("Couldn't add migrated emote on: "
+                                + dstChan.getName() + "/#" + msg.getId().asString(), err);
+                        fut.complete(createdMessage);
+                    })
                     .subscribe(),
                 fut::completeExceptionally);
         return Mono.fromFuture(fut);
